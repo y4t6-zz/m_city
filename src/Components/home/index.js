b90d31d69111e3287ced0446bcfd68f9d5b96832
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-03 14:47:16 +0300

    Start creating components for home

---
+++ src/Components/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Featured from './featured';
+
+const Home = (props) => {
+    return (
+        <div className='bck_blue'>
+            <Featured/>
+        </div>
+    );
+};
+
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Components/home';
 
 const Routes = (props) => {
   return(
       <Layout>
-          Second Line
+          <Switch>
+              <Route exact component={Home} path='/' />
+          </Switch>
       </Layout>
   )
 }
