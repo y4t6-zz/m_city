7eb66d2b42e52d3098a48cb6c0181b03e95c3542
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-03 20:45:28 +0300

    Add reusable components

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './featured';
+import Matches from './matches';
 
 const Home = (props) => {
     return (
         <div className='bck_blue'>
             <Featured/>
+            <Matches/>
         </div>
     );
 };
---
+++ src/Components/home/matches/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Tag } from '../../ui/misc';
+
+const MatchesHome = () => {
+    return (
+        <div className='home_matches_wrapper'>
+            <div className='container'>
+                <Tag
+                    bck='#0e1731'
+                    size='50px'
+                    color='#fff'
+                >
+                    Matches
+                </Tag>
+
+                BLOCKS
+
+                <Tag
+                    bck='#fff'
+                    size='22px'
+                    color='#0e1731'
+                    link={true}
+                    linkTo='/the_team'
+                >
+                    See more matches
+                </Tag>
+            </div>
+        </div>
+    );
+};
+
+export default MatchesHome;
