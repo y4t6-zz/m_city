78cfea2e1058f542fe6b5eeba9bc02970a2fc45f
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-03 21:53:54 +0300

    Finish the matches block

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,9 @@ firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
 
-firebaseDB.ref('matches').once('value').then((snapshot)=>{
-    console.log(snapshot.val())
-})
+const firebaseMatches = firebaseDB.ref('matches');
+
+export {
+    firebase,
+    firebaseMatches
+}
