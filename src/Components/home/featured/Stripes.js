9bf0e5a24d4741b2bf284f9a022016ee473cf9df
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-03 20:28:53 +0300

    Add featured animation

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -4,10 +4,75 @@ import Animate from 'react-move/Animate';
 
 class Stripes extends Component {
 
+    state = {
+        stripes: [
+            {
+                background: '#98c5e9',
+                left: 120,
+                rotate: 25,
+                top: -260,
+                delay: 0
+            },
+            {
+                background: '#fff',
+                left: 360,
+                rotate: 25,
+                top: -397,
+                delay: 200
+            },
+            {
+                background: '#98c5e9',
+                left: 600,
+                rotate: 25,
+                top: -498,
+                delay: 400
+            }
+        ]
+    }
+
     showStripes = () => (
-        <div>
-            Stripes
-        </div>
+        this.state.stripes.map((stripe, index) => (
+            <Animate
+                key={index}
+                show={true}
+
+                start={{
+                    background: '#fff',
+                    opacity: 0,
+                    left: 0,
+                    rotate: 0,
+                    top: 0
+                }}
+
+                enter={{
+                    background: [stripe.background],
+                    opacity: [1],
+                    left: [stripe.left],
+                    rotate: [stripe.rotate],
+                    top: [stripe.top],
+                    timing: {delay: stripe.delay, duration: 200, ease: easePolyOut},
+                    events: {
+                        end() {
+
+                        }
+                    }
+                }}
+            >
+                {({ opacity,left,rotate,top,background })=>{
+                    return(
+                        <div
+                            className='stripe'
+                            style={{
+                                background,
+                                opacity,
+                                transform: `rotate(${rotate}deg) translate(${left}px, ${top}px)`,
+
+                            }}
+                        ></div>
+                    )
+                }}
+            </Animate>
+        ))
     )
 
     render() {
