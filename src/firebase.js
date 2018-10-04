13aab21a7aa9a178dbba9169525351be01949e50
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-04 12:33:12 +0300

    Delete firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/app';
 import 'firebase/database';
 
 const config = {
-    apiKey: "AIzaSyByc9Kf1soCa9oKJlYvCrL3X51_uQh9Dp4",
-    authDomain: "m-city-e0ac5.firebaseapp.com",
-    databaseURL: "https://m-city-e0ac5.firebaseio.com",
-    projectId: "m-city-e0ac5",
-    storageBucket: "m-city-e0ac5.appspot.com",
-    messagingSenderId: "596442138120"
+
 };
 
 firebase.initializeApp(config);
