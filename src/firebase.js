efefa4ff2143085fc473812a70693498c8f28fad
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-05 19:43:27 +0300

    Add signin component

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
+import 'firebase/auth';
 import { getFirebaseConfig } from './secrets';
 
 const config = getFirebaseConfig();
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Components/home';
+import SignIn from './Components/signin';
 
 const Routes = (props) => {
   return(
       <Layout>
           <Switch>
+              <Route exact component={SignIn} path='/sign_in' />
               <Route exact component={Home} path='/' />
           </Switch>
       </Layout>
