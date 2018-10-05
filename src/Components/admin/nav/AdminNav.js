fcc2f665933b2582700548242846531a1de0fed3
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-05 20:45:46 +0300

    Add AdminNav component

---
+++ src/Components/admin/nav/AdminNav.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import { firebase } from '../../../firebase';
+
+const AdminNav = () => {
+
+    const links = [
+        {
+            title: 'Matches',
+            linkTo: '/admin_matches'
+        },
+        {
+            title: 'Add Match',
+            linkTo: '/admin_matches/add_match'
+        },
+        {
+            title: 'Players',
+            linkTo: '/admin_players'
+        },
+        {
+            title: 'Add Players',
+            linkTo: '/admin_players/add_players'
+        }
+    ]
+
+    const styles = {
+        color: '#fff',
+        fontWeight: 300,
+        borderBottom: '1px solid #353535'
+    }
+
+    const renderItems = () => (
+        links.map((link, index) => (
+            <Link to={link.linkTo} key={index}>
+                <ListItem button style={styles}>
+                    {link.title}
+                </ListItem>
+            </Link>
+        ))
+    )
+
+    const logoutHandler = () => {
+        firebase.auth().signOut().then(()=>{
+            console.log('Log out succesfull!!')
+        },(error)=>{
+            console.log('Error login out!')
+        })
+    }
+
+    return (
+        <div>
+            {renderItems()}
+            <ListItem button style={styles} onClick={()=> logoutHandler()}>
+                Log out
+            </ListItem>
+        </div>
+    );
+};
+
+export default AdminNav;
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 
+import Dashboard from './Components/admin/Dashboard';
+
 const Routes = (props) => {
   return(
       <Layout>
           <Switch>
+              <Route exact component={Dashboard} path='/dashboard' />
               <Route exact component={SignIn} path='/sign_in' />
               <Route exact component={Home} path='/' />
           </Switch>
